445498f7bb7cbf14e79f5328b95e9d90db420637
Jose Ernesto Carreno Bueno <josernesto1989@gmail.com>
2022-08-02 16:22:35 -0400

    sin webhook

--- bot.py
+++ bot.py
@@ -23,14 +23,15 @@ app.add_handler(CommandHandler("ayuda", help))
 
 app.add_handler(CommandHandler("preciosDivisas", getPrecioMLC))
 
+app.run_polling()
 
 # app.run_webhook(listen="0.0.0.0", port=PORT, url_path=os.getenv("TOKEN"))
 # app.bot.setWebhook('https://mlctelegrambot.herokuapp.com/' + os.getenv("TOKEN"))
 # app.run_polling()
-app.run_webhook(
-    listen='0.0.0.0',
-    port=5000,
-    url_path='TOKEN',
-    webhook_url='https://mlctelegrambot.herokuapp.com/'+os.getenv("TOKEN")
-    # cert='cert_bot1.pem'
-    )
+# app.run_webhook(
+#     listen='0.0.0.0',
+#     port=5000,
+#     url_path='TOKEN',
+#     webhook_url='https://mlctelegrambot.herokuapp.com/'+os.getenv("TOKEN")
+#     # cert='cert_bot1.pem'
+#     )
